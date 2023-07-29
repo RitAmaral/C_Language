ef5a7980fe1d066fc87a8a7ec99dc3e81d8d7427
Rita <132366922+RitAmaral@users.noreply.github.com>
2023-07-29 11:33:26 +0100

    update

--- BeginningC/main.c
+++ BeginningC/main.c
@@ -6,17 +6,20 @@ int main()
     printf("Hello world!\n"); //printf imprime Hello world! na consola
 
     //variáveis
-    char nome[] = "Rita"; // temos de colocar [] porque queremos colocar vários caracteres
+    char nome[] = "Rita"; // temos de colocar [] porque queremos colocar vários caracteres; string entre ", e uma letra entre '
     int idade = 27;
 
     printf("My name is %s, and I'm %d years old.\n", nome, idade); //o %s vai buscar char, mas temos de colocar o nome da variável à frente.
     // o %d vai buscar os int.
 
     //outros tipos de dados:
-    double numero = 19.02; //%f vai buscar
-    char letra = 'A';
+    double numero = 19.02; //%f vai buscar floating points/números decimais
+    char letra = 'A'; // %c vai buscar chars
 
-    printf("My favorite %s is %f, and my favorite letter is %c.\n", "number", numero, letra);
+    printf("This is a %s: %f, and this is a char: %c.\n", "floating point or real number", numero, letra);
+
+    printf("Floating points like: %.2f usually takes twice as much memory as an integer like: %d.\n", numero, idade);
+    // %.2f mostra o número decimal com duas casas decimais
 
     return 0;
 }
