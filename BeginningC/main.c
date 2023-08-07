4d18606463e61db2444a844a83f4fa4ef622dd97
Rita <132366922+RitAmaral@users.noreply.github.com>
2023-08-07 17:57:19 +0100

    constantes, bool, if else, switch

--- BeginningC/main.c
+++ BeginningC/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int main()
 {
@@ -19,7 +20,7 @@ int main()
 
     printf("This is a %s: %f, and this is a char: %c.\n", "floating point or real number", numero, letra);
 
-    printf("Floating points like: %.2f usually takes twice as much memory as an integer like: %d.\n", numero, idade);
+    printf("Floating points like: %.2f usually takes twice as much memory as an integer like: %d.\n\n", numero, idade);
     // %.2f mostra o número decimal com duas casas decimais
 
     /* Tamanho que ocupam os tipos de dados:
@@ -28,6 +29,7 @@ int main()
         double: 8 bytes
         char: 1 byte
     */
+
 
     /* Format Specifiers:
         %c vai buscar char
@@ -38,12 +40,14 @@ int main()
 
 
     //Escape sequences
+    /*
     printf("\nEscape sequence \n = nova linha \n");
     printf("Escape sequence \t = um tab \n");
     printf("Escape sequence \a = um alert e emitido quando executado o programa \n");
     printf("Escape sequence \\ = insere a barra \\ \n");
     printf("Escape sequence \' = insere pelicas \n");
     printf("Escape sequence \" = insere aspas \n\n");
+    */
 
 
     //Somar variáveis
@@ -65,6 +69,86 @@ int main()
 
     printf("%f \n", divisao2); //output é 2.500000
     printf("%.2f \n\n", divisao2); //output é 2.50
+
+    //Constantes - quando não queremos que um número seja alterado / read-only
+    const float NUMPI = 3.14; //é boa prática escrever constantes com letras maiúsculas
+
+    printf("%.2f \n\n", NUMPI);
+    //NUMPI = 5; //isto dá um erro
+
+    //Operadores
+    int sum1 = 100 + 50; //estes e casos são possíveis
+    int sum2 = sum1 + 250;
+    int sum3 = sum2 + sum2;
+
+    printf("%i \n", sum1); // 150 (100 + 50)
+    printf("%i \n", sum2); // 400 (150 + 250)
+    printf("%i \n\n", sum3); // 800 (400 + 400)
+
+    /* Operadores
+     +     -     *   /     %    ++x (aumenta a variável +1)    --x (diminui a variável -1)
+     +=    -=   *=   /=
+     >     <
+    */
+
+    int x1 = 5;
+    int y1 = 3;
+    printf("%d ", x1 > y1); // returns 1 (true) porque 5 é maior que 3
+    printf("%d ", x1 < y1); // returns 0 (false) porque 3 não é maior que 5
+    printf("%d ", x1 == y1); // returns 0 (false) porque 3 não é igual a 5
+    printf("%d \n\n", x1 != y1); // returns 1 (true) porque 5 é diferente de 3
+
+    //operadores lógicos: && (e)    || (ou)     !
+
+    //variáveis do tipo bool, incluir lá em cima <stdbool.h>
+    bool isProgrammingFun = true;
+    bool isCBoring = false;
+
+    printf("%d ", isProgrammingFun); // 1 = true, usar formato %d para imprimir a variável bool
+    printf("%d ", isCBoring); // 0 = false
+
+    printf("%d \n\n", 10 > 9);  // Returns 1 (true) porque 10 é maior que 9
+
+
+    //if else
+    int idade1 = 27;
+    int idadeConduzir = 18;
+
+    if (idade1 >= idadeConduzir) {
+        printf("Podes conduzir \n\n");
+    } else {
+        printf("Não podes conduzir \n\n");
+    }
+
+    int hora = 18;
+    if (hora <= 12) {
+      printf("Bom dia.");
+    } else if (hora <= 20) {
+      printf("Boa tarde.");
+    } else {
+      printf("Boa noite.");
+    }
+    // Outputs "Boa tarde."
+
+    //outra forma de fazer if/else:
+    (hora <= 17) ? printf("Bom dia. \n\n") : printf("Boa noite. \n\n");
+
+
+    //switch
+    int dia = 4;
+
+    switch (dia) {
+      case 1:
+        printf("Hoje é sábado");
+        break;
+      case 2:
+        printf("Hoje é domingo");
+        break;
+      default:
+        printf("Quase fim-de-semana!");
+    } // Outputs "Quase fim-de-semana!"
+
+
 
 
     printf("\n");
