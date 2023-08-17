8abec31eb206b6353b7a2bd00ba479446dfa8cbc
Rita <132366922+RitAmaral@users.noreply.github.com>
2023-08-17 18:35:09 +0100

    pointers

--- PointersC/main.c
+++ PointersC/main.c
@@ -12,7 +12,10 @@ int main()
 
     //variáveis normais:
     int qua;
-    float preco;
+    float preco = 10.50;
+
+    printf("%.2f\n", preco); //output é o valor de preco = 10.50
+    printf("%p\n\n", &preco); //output é a morada da memória = 000000000061FE08
 
     //Definir pointers:
     int * pQua; //colocar * e um p de pointer
@@ -40,8 +43,28 @@ int main()
      printf("%d\n", age); //output é 25
 
      *pAge = 25;
-     printf("%d\n", *pAge); //output é 25, logo armazena 25 onde aponta, que é para a memória do age.
-     printf("%d\n", pAge); //isto dá a morada de pAge que é 6422024.
+     printf("%d\n", *pAge); //output é 25, logo armazena 25 onde aponta, que é para a memória do age. => dereferenciar
+     printf("%d\n\n", pAge); //isto dá a morada de pAge que é 6422024.
+
+
+     //Pointers e Arrays
+
+     int myNumbers[5] = {0, 25, 50, 75, 100};
+     int *ptr = myNumbers; //ligação ao pointer
+     int i;
+
+     for (i = 0; i < 5; i++) {
+        printf("%d\n", *(ptr + i)); //dereferenciar, e somar + i para print todos os elementos do array
+     } //output é 0 25 50 75 100
+
+     //podemos mudar os elementos assim:
+     myNumbers[0] = 1;
+     printf("\n%d\n", myNumbers[0]);
+
+     //ou com pointers assim:
+     *(myNumbers + 1) = 26;
+     printf("%d\n", *(myNumbers + 1));
+
 
     return 0;
 }
